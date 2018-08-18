8e470518cfe3d8eb07e42a00c51f4dbab2249775
asgerb <a@asgerbehnckejacobsen.dk>
2018-08-18 20:05:21 +0200

    Simplify test

--- test/mongoid_localized_fields_fallback_patch_test.rb
+++ test/mongoid_localized_fields_fallback_patch_test.rb
@@ -3,17 +3,11 @@ require "test_helper"
 describe MongoidLocalizedFieldsFallbackPatch do
   it { ::MongoidLocalizedFieldsFallbackPatch::VERSION.wont_be_nil }
 
-  let(:field) { Mongoid::Fields::Localized.new(:description, localize: true, type: String) }
+  describe "when the current locale is an empty string" do
+    before { I18n.locale = :de }
+    let(:field) { Mongoid::Fields::Localized.new(:description, localize: true, type: String) }
+    let(:localized_value) { field.demongoize({ "de" => "", "es" => "pruebas" }) }
 
-  before do
-    I18n.locale = :de
-  end
-
-  describe "fallbacks" do
-    describe "when the current locale is an empty string" do
-      let(:localized_value) { field.demongoize({ "de" => "", "es" => "pruebas" }) }
-
-      it { localized_value.must_equal "pruebas" }
-    end
+    it { localized_value.must_equal "pruebas" }
   end
 end
